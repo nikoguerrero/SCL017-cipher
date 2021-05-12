982c93d9f7c674c785579c461da3a7007d1aa052
Niko Guerrero <nclguerrero@gmail.com>
2021-05-12 16:38:54 -0400

    añadido código césar básico

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+  encode: function(offset, string) {
+
+    // charCode -> UNICODE (ASCII hasta 128)
+    let charCodeArray = [];
+      
+    for(let i = 0; i < string.length; i++){
+        let character = string.charCodeAt(i);
+        let code = (character - 65 + offset) % 26 + 65;
+        charCodeArray += String.fromCharCode(code);
+    }
+    return charCodeArray;
+  }
 };
 
+cipher.decode = function(offset, string) {
+    let charCodeArray = [];
+      
+    for(let i = 0; i < string.length; i++){
+        let character = string.charCodeAt(i);
+        let code = (character - 65 - offset) % 26 + 65;
+        charCodeArray += String.fromCharCode(code);
+    }
+    return charCodeArray;
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -6,31 +6,21 @@ let codeButton = document.getElementById("codeButton");
 let decodeButton = document.getElementById("decodeButton");
 let userText1 = document.getElementById("userText1");
 let userText2 = document.getElementById("userText2");
-
+let shiftText = document.getElementById("shift");
 
 codeButton.addEventListener("click", clickCode);
 decodeButton.addEventListener("click", clickDecode);
 
-
 function clickCode() {
+    let offset = parseInt(shiftText.value);
     let firstInputText = userText1.value;
-
-    // charCode -> UNICODE (ASCII hasta 128)
-    let charCodeArray = [];
-    
-    for(let i = 0; i < firstInputText.length; i++){
-        let character = firstInputText.charCodeAt(i);
-        let offset = 30;
-        let code = (character - 65 + offset) % 26 + 65;
-        charCodeArray += String.fromCharCode(code);
-    }
-    console.log(charCodeArray);
-    userText2.value = charCodeArray;
-}
+    userText2.value = cipher.encode(offset, firstInputText);
+}   
 
 function clickDecode() {
+    let offset = parseInt(shiftText.value);
     let secondInputText = userText2.value;
-    userText1.value = secondInputText;
+    userText1.value = cipher.decode(offset, secondInputText);
 }
 
 
