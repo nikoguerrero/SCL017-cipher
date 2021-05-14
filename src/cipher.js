6f6a1e222adee817ca4fba67cca7aaa2615feef6
Niko Guerrero <nclguerrero@gmail.com>
2021-05-14 11:06:36 -0400

    Ordenamiento de esqueleto HTML. Corrección de fórmula desplazamiento para mayúsculas. Activación y prueba de todos los tests

--- src/cipher.js
+++ src/cipher.js
@@ -7,7 +7,7 @@ const cipher = {
     if(typeof string !== "string") {
       throw new TypeError("error en valor de string");
     }
-    
+
     // charCode -> UNICODE (ASCII hasta 128)
     let charCodeArray = [];
       
@@ -34,13 +34,15 @@ const cipher = {
     }
 
     let charCodeArray = [];
-      
+    
     for(let i = 0; i < string.length; i++){
       let character = string.charCodeAt(i);
+      console.log(((character - 97 + offset) % 26) + 97);
+
       if(character >= 97 && character <= 122) {
-        charCodeArray += String.fromCharCode((character - 97 - offset)% 26 + 97);
+        charCodeArray += String.fromCharCode(((character - 97 + offset) % 26) + 97);
       } else if(character >= 65 && character <= 90) {
-        charCodeArray += String.fromCharCode((character - 65 - offset)% 26 + 65);
+        charCodeArray += String.fromCharCode((character + 65 - offset) % 26 + 65);
       } else {
         charCodeArray += String.fromCharCode(character);
       }
