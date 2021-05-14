96265d18ece80e32179a16c5cd9e57947b1e3341
Niko Guerrero <nclguerrero@gmail.com>
2021-05-13 23:10:24 -0400

    implementación completa de minúsculas en encriptación y de mensaje TypeError

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,52 @@
 const cipher = {
   encode: function(offset, string) {
+    if(typeof offset !== "number") {
+      throw new TypeError("error en valor de offset");
+    }
 
+    if(typeof string !== "string") {
+      throw new TypeError("error en valor de string");
+    }
+    
     // charCode -> UNICODE (ASCII hasta 128)
     let charCodeArray = [];
       
     for(let i = 0; i < string.length; i++){
         let character = string.charCodeAt(i);
-        let code = (character - 65 + offset) % 26 + 65;
-        charCodeArray += String.fromCharCode(code);
+        if(character >= 97 && character <= 122) {
+          charCodeArray += String.fromCharCode((character - 97 + offset)% 26 + 97);
+        } else if(character >= 65 && character <= 90) {
+          charCodeArray += String.fromCharCode((character - 65 + offset)% 26 + 65);
+        } else {
+          charCodeArray += String.fromCharCode(character);
+        }
+    }
+    return charCodeArray;
+  },
+  
+  decode: function(offset, string) {
+    if(typeof offset !== "number") {
+      throw new TypeError("error en valor de offset");
+    }
+
+    if(typeof string !== "string") {
+      throw new TypeError("error en valor de string");
+    }
+
+    let charCodeArray = [];
+      
+    for(let i = 0; i < string.length; i++){
+      let character = string.charCodeAt(i);
+      if(character >= 97 && character <= 122) {
+        charCodeArray += String.fromCharCode((character - 97 - offset)% 26 + 97);
+      } else if(character >= 65 && character <= 90) {
+        charCodeArray += String.fromCharCode((character - 65 - offset)% 26 + 65);
+      } else {
+        charCodeArray += String.fromCharCode(character);
+      }
     }
     return charCodeArray;
   }
 };
 
-cipher.decode = function(offset, string) {
-    let charCodeArray = [];
-      
-    for(let i = 0; i < string.length; i++){
-        let character = string.charCodeAt(i);
-        let code = (character - 65 - offset) % 26 + 65;
-        charCodeArray += String.fromCharCode(code);
-    }
-    return charCodeArray;
-};
 export default cipher;
