b0a6b7f44cd459a9ce9c3b54ff0c7de1951bc857
Niko Guerrero <nclguerrero@gmail.com>
2021-05-14 13:53:17 -0400

    corregido cipher.js para correr test de offset 33 con minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -37,10 +37,9 @@ const cipher = {
     
     for(let i = 0; i < string.length; i++){
       let character = string.charCodeAt(i);
-      console.log(((character - 97 + offset) % 26) + 97);
 
       if(character >= 97 && character <= 122) {
-        charCodeArray += String.fromCharCode(((character - 97 + offset) % 26) + 97);
+        charCodeArray += String.fromCharCode((character + 97 + offset) % 26 + 97);
       } else if(character >= 65 && character <= 90) {
         charCodeArray += String.fromCharCode((character + 65 - offset) % 26 + 65);
       } else {
