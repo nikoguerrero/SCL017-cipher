e624b6053f50030b31ae1ec053116e9fb3b23b60
Niko Guerrero <nclguerrero@gmail.com>
2021-05-19 15:10:31 -0400

    cambio de variables let a const en index

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-let codeButton = document.getElementById("codeButton");
-let decodeButton = document.getElementById("decodeButton");
-let userText1 = document.getElementById("userText1");
-let userText2 = document.getElementById("userText2");
-let shiftText = document.getElementById("shift");
+const codeButton = document.getElementById("codeButton");
+const decodeButton = document.getElementById("decodeButton");
+const userText1 = document.getElementById("userText1");
+const userText2 = document.getElementById("userText2");
+const shiftText = document.getElementById("shift");
 
 codeButton.addEventListener("click", clickCode);
 decodeButton.addEventListener("click", clickDecode);
 
 function clickCode() {
-    let offset = parseInt(shiftText.value);
-    let firstInputText = userText1.value;
+    const offset = parseInt(shiftText.value);
+    const firstInputText = userText1.value;
     userText2.value = cipher.encode(offset, firstInputText);
 }   
 
 function clickDecode() {
-    let offset = parseInt(shiftText.value);
-    let secondInputText = userText2.value;
+    const offset = parseInt(shiftText.value);
+    const secondInputText = userText2.value;
     userText1.value = cipher.decode(offset, secondInputText);
 }
 
