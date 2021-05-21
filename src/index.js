7f6b02d54cff42d3f52aef16e6499fdb881e9060
Niko Guerrero <nclguerrero@gmail.com>
2021-05-21 16:18:25 -0400

    nuevas funciones para ingresar y volver de pantalla de instrucciones

--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ const decodeButton = document.getElementById("decodeButton");
 const userText1 = document.getElementById("userText1");
 const userText2 = document.getElementById("userText2");
 const shiftText = document.getElementById("shift");
+const about = document.getElementById("about");
+const instructions = document.getElementById("instructions");
+const caesarCipher = document.getElementById("caesarCipher");
 
 codeButton.addEventListener("click", clickCode);
 decodeButton.addEventListener("click", clickDecode);
+about.addEventListener("click", aboutLink);
+instructions.addEventListener("click", backLink);
+
+
+instructions.style.display = "none";
 
 function clickCode() {
     const offset = parseInt(shiftText.value);
@@ -23,6 +31,15 @@ function clickDecode() {
     userText1.value = cipher.decode(offset, secondInputText);
 }
 
+function aboutLink() {
+    caesarCipher.style.display = "none";
+    instructions.style.display = "block";
+}
+
+function backLink() {
+    instructions.style.display = "none";
+    caesarCipher.style.display = "block";
+}
 
 
 
