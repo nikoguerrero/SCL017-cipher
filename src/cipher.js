8b04d20e84c14e44271b3234a31a589c99533525
Niko Guerrero <nclguerrero@gmail.com>
2021-05-24 20:12:18 -0400

    implementación de función de cifrado genérico César

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,30 @@
+function caesarCipher (character, offset) {
+  const characterLowerCase = character >= 97 && character <= 122;
+  const characterUpperCase = character >= 65 && character <= 90;
+  const characterNumber = character >= 48 && character <= 57;
+
+  if(characterLowerCase || characterUpperCase || characterNumber) {
+  
+    let modulo = 26;
+    let ascii = 65;
+
+    if(characterLowerCase) {
+      ascii = 97;
+    } else if(characterNumber) {
+      modulo = 10;
+      ascii = 48;
+    }
+
+    let charModulo = ((character - ascii) + offset) % modulo;
+    if(charModulo < 0) {
+      charModulo = charModulo + modulo;
+    }
+    return String.fromCharCode((charModulo + ascii));
+  } else {
+    return String.fromCharCode(character);
+  }
+}
+
 const cipher = {
   encode: function(offset, string) {
     if(typeof offset !== "number") {
@@ -14,30 +41,7 @@ const cipher = {
     for(let i = 0; i < string.length; i++){
 
         const character = string.charCodeAt(i);
-        const characterLowerCase = character >= 97 && character <= 122;
-        const characterUpperCase = character >= 65 && character <= 90;
-        const characterNumber = character >= 48 && character <= 57;
-
-        if(characterLowerCase || characterUpperCase || characterNumber) {
-        
-          let modulo = 26;
-          let ascii = 65;
-    
-          if(characterLowerCase) {
-            ascii = 97;
-          } else if(characterNumber) {
-            modulo = 10;
-            ascii = 48;
-          }
-  
-          let charModulo = ((character - ascii) + offset) % modulo;
-          if(charModulo < 0) {
-            charModulo = charModulo + modulo;
-          }
-          output += String.fromCharCode((charModulo + ascii));
-        } else {
-          output += String.fromCharCode(character);
-        }
+        output += caesarCipher(character, offset);
     }
     return output;
   },
@@ -56,34 +60,11 @@ const cipher = {
     for(let i = 0; i < string.length; i++){
 
       let character = string.charCodeAt(i);
-      const characterLowerCase = character >= 97 && character <= 122;
-      const characterUpperCase = character >= 65 && character <= 90;
-      const characterNumber = character >= 48 && character <= 57;
-
-
-      if(characterLowerCase || characterUpperCase || characterNumber) {
-        
-        let modulo = 26;
-        let ascii = 65;
-  
-        if(characterLowerCase) {
-          ascii = 97;
-        } else if(characterNumber) {
-          modulo = 10;
-          ascii = 48;
-        }
-
-        let charModulo = ((character - ascii) - offset) % modulo;
-        if(charModulo < 0) {
-          charModulo = charModulo + modulo;
-        }
-        output += String.fromCharCode((charModulo + ascii));
-      } else {
-        output += String.fromCharCode(character);
-      }
+      output += caesarCipher(character, -offset);
     }
     return output;
   }
 };
 
 export default cipher;
+
